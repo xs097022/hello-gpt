52d8f70f19a4f1bb1fd5fc430c254f194653ffae
qiyu <xs097022@163.com>
2023-04-02 19:24:59 +0800

    test

--- 1.js
+++ 1.js
@@ -1,5 +1,63 @@
 const extensionId = 'cnhfonpeambacbnpcmjmnckcalamhadn';
 
+const Msg = ((exports) => {
+    const t = document.getElementById('msg');
+    exports.set = (msg) => {
+        t.innerHTML = msg;
+    };
+    return exports;
+})({});
+
+const Canvas = ((exports) => {
+    const t = document.getElementById('canvas');
+    exports.set = (fn) => {
+        t.innerHTML =  (Store.Map.logList || []).filter(fn).map((i) => {
+            return `
+            <div class="tr">
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <i>RASA命中22</i>
+                    <i>GPT命中22</i>
+                </div>
+                <div>
+                    <button>标记</button>
+                </div>
+            </div>
+            `;
+        });
+    };
+    return exports;
+})({});
+
+const Filter = ((exports) => {
+    const t = document.getElementById('filter');
+    [].forEach.call(t.querySelectorAll('i'), (i) => {
+        i.addEventListener('click', (e) => {
+            if(!i.getAttribute('selected')) {
+                t.querySelector('[selected]').removeAttribute('selected');
+                i.setAttribute('selected', true);
+                const t = [
+                    i => !i.rasa && !i.check0,
+                    i => i.rasa && !i.check0,
+                    i => !i.rasa && i.check0 && !i.check1.length,
+                    i => i.rasa && i.check0 && !i.check1.length,
+                    i => !i.rasa && i.check0 && i.check1.length,
+                    i => i.rasa && i.check0 && i.check1.length && i.check1.indexOf(i.rasa) === -1,
+                    i => i.rasa && i.check0 && i.check1.length && i.check1.indexOf(i.rasa) !== -1,
+                    i => i.flag
+                ][i.dataset.index];
+                Canvas.set(t);
+            }
+        });
+    });
+    return exports;
+})({});
+
 const Store = ((exports) => {
     exports.Map = {};
     exports.MM = () => {};
@@ -146,14 +204,24 @@ const main0 = async () => {
             return ['0', undefined];
         }
     };
+    const wrapGptCheck0 = async (msg) => {
+        const _msg = await gptApi.say(Store.Map.Check0Msg.replace(/{MSG}/g, msg));
+        return /1/.test(_msg);
+    };
+    const wrapGptCheck1 = async (msg) => {
+        const _msg = await gptApi.say(Store.Map.Check1Msg.replace(/{MSG}/g, msg));
+        return /1/.test(_msg);
+    };
     const Map0 = {};
     Map0['0'] = async () => {
         const [status, msg] = await wrapApiLogin();
         return [status, msg];
     };
     Map0['0-0'] = () => {
+        Msg.set('同步失败')
     };
     Map0['0-1'] = async (msg) => {
+        Msg.set('开始同步问答库')
         Store.Map.authHeaders = {
             Authorization: msg
         };
@@ -162,16 +230,20 @@ const main0 = async () => {
         return [status, _msg];
     };
     Map0['0-1-0'] = () => {
+        Msg.set('同步问答库失败')
     };
     Map0['0-1-1'] = async (msg) => {
+        Msg.set('开始同步问答记录')
         Store.Map.qaList = msg;
         Store.MM();
         const [status, _msg] = await wrapApiLogList();
         return [status, _msg];
     };
     Map0['0-1-1-0'] = () => {
+        Msg.set('同步问答记录失败')
     };
     Map0['0-1-1-1'] = async (msg) => {
+        Msg.set('开始RASA命中')
         Store.Map._logList = msg;
         const Map = Store.Map.qaList.reduce((ret, i) => {
             i.qs.forEach(ii => {
@@ -193,6 +265,7 @@ const main0 = async () => {
                     const [_, x] = await rasaApi.modelParse(Store.Map.logList[nn].q);
                     try {
                         Store.Map.logList.rasa = JSON.parse(x);
+                        Store.MM();
                     } catch(e) {
                         n = 0;
                         ret[0] = '0';
@@ -205,25 +278,66 @@ const main0 = async () => {
         return ret;
     };
     Map0['0-1-1-1-0'] = () => {
+        Msg.set('RASA命中失败')
     };
     Map0['0-1-1-1-1'] = async () => {
-        const ret = ['0', undefined];
+        Msg.set('开始GPT相关')
+        const ret = ['1', undefined];
         const fn = ((n) => {
             return async () => {
                 if(n === 0) {
                     return undefined;
                 } else {
                     const nn = --n;
-                    const x = await gptApi.check0(Store.Map.logList[nn].q);
-                    const xx = await gptApi.check1(Store.Map.logList[nn].q);
-                    console.log(x);
+                    const x = Store.Map.logList[nn].check0 = await gptApi.check0(Store.Map.logList[nn].q);
+                    if(x) {
+                    } else {
+                        Store.Map.logList[nn].check1 = [];
+                    }
+                    Store.MM();
                 }
             };
         })(Store.Map.logList.length);
         await Batch.run(100, fn);
         return ret;
     };
-    Run.run(MapFn.new(Map0), '0', (p, status) => p + '-' + status, {}, () => {});
+    Map0['0-1-1-1-1-0'] = () => {
+        Msg.set('GPT相关失败');
+    };
+    Map0['0-1-1-1-1-1'] = async () => {
+        Msg.set('开始GPT命中');
+        const ret = ['1', undefined];
+        const fn = ((n) => {
+            return async () => {
+                if(n === 0) {
+                    return undefined;
+                } else {
+                    const nn = --n;
+                    const x = Store.Map.logList[nn].check0 = await gptApi.check0(Store.Map.logList[nn].q);
+                    if(x) {
+                    } else {
+                        Store.Map.logList[nn].check1 = [];
+                    }
+                    Store.MM();
+                }
+            };
+        })(Store.Map.logList.length);
+        await Batch.run(2, fn);
+        return ret;
+    };
+    Map0['0-1-1-1-1-1-0'] = () => {
+        Msg.set('GPT命中失败');
+    };
+    Map0['0-1-1-1-1-1-1'] = () => {
+        Msg.set('GPT命中结束');
+    };
+    //Run.run(MapFn.new(Map0), '0', (p, status) => p + '-' + status, {}, () => {});
+    document.getElementById('start').addEventListener('click', () => {
+        Store.Map = {};
+        Store.MM();
+        Msg.set('开始');
+        Run.run(MapFn.new(Map0), '0', (p, status) => p + '-' + status, {}, () => {});
+    });
 };
 
 main0();
